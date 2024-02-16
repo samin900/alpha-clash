6b2a2eac8da850c701fddd47c11110c0e10aef72
samin900 <saminahmed900@Gmail.com>
2024-02-16 21:49:54 +0600

    showing alphabet

--- scripts/alpha.js
+++ scripts/alpha.js
@@ -1,3 +1,50 @@
+
+function continueGame(){
+    const alphabet = getAAlphabet();
+    console.log(alphabet)
+
+
+    const currentAlphabetElement = document.getElementById('current-alphabet')
+    currentAlphabetElement.innerText = alphabet;
+
+    setBackgroundColor(alphabet)
+}
+
+
+
 function play() {
-console.log('play now')
+
+const homeSection = document.getElementById('home')
+homeSection.classList.add('hidden')
+
+
+const playSection = document.getElementById('play')
+playSection.classList.remove('hidden')
+
+continueGame();
+
+}
+
+function setBackgroundColor  (elementId){
+    const element = document.getElementById(elementId)
+    element.classList.add('bg-orange-400')
+}
+
+
+
+function getAAlphabet(){
+    const alphaString = 'abcdefghijklmnopqrstuvwxyz';
+const alphabets = alphaString.split('')
+console.log(alphabets)
+
+
+const randomNumber = Math.random() * 25;
+const index = Math.round(randomNumber);
+
+
+
+const alphabet = alphabets[index]
+console.log(index, alphabet);
+return alphabet;
+
 }
